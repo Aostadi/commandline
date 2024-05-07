b36c4fe12078a2e33560c00474787f9bc30aae2b
MrRezoo <rezam578@gmail.com>
2024-05-07 22:16:14 +0330

    Add AbstractFactory

--- DesignPatterns/Singletone/singleton.py
+++ DesignPatterns/Singletone/singleton.py
@@ -1,19 +1,48 @@
-class Singleton:
+"""
+   Creational:
+        Singleton
+"""
 
-    @classmethod
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(*args, **kwargs)
-        return cls.instance
+
+class Singleton1(type):
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__call__()
+        return cls._instance
+
+
+class Singleton2(type):
+    """
+    Singleton metaclass
+    Based on Python Cookbook 3rd Edition Recipe 9.13
+    Only one instance of a class can exist. Does not work with __slots__
+    """
+
+    def __init__(self, *args, **kw):
+        super(Singleton2, self).__init__(*args, **kw)
+        self.__instance = None
+
+    def __call__(self, *args, **kw):
+        if self.__instance is None:
+            self.__instance = super(Singleton2, self).__call__(*args, **kw)
+        return self.__instance
+
+
+class DBMongo(metaclass=Singleton2):
+    pass
+
+class DBPostgres(metaclass=Singleton2):
+    pass
 
 
 if __name__ == '__main__':
-    s1 = Singleton()
-    s2 = Singleton()
-    s3 = Singleton()
+    m1 = DBMongo()
+    m2 = DBMongo()
 
-    print(id(s1))
-    print(id(s2))
-    print(id(s3))
+    p1 = DBPostgres()
+    p2 = DBPostgres()
 
-    print(id(s1) == id(s2) == id(s3))
+    print(f"Mongo: {id(m1)} - {id(m2)} ", id(m1) == id(m2))
+    print(f"Postgres: {id(p1)} - {id(p2)} ", id(p1) == id(p2))
