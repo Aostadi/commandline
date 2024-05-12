a7cfe39c5d8451886ad703704eff65b8feeece38
MrRezoo <rezam578@gmail.com>
2024-05-12 12:49:07 +0330

    Add Adapter Pattern

---
+++ DesignPatterns/Adapter/course_code.py
@@ -0,0 +1,26 @@
+"""
+    Structural:
+        Adapter => 1.Adaptee 2.Adapter 3.Client
+"""
+from DesignPatterns.AbstractFactory.course_code import Rugs
+
+
+class PriceAdapter:
+    def __init__(self, rate):
+        self.rate = rate
+
+    def exchange(self, product):
+        return self.rate * product._price
+
+
+if __name__ == '__main__':
+    r1 = Rugs('persian rugs', 1200)
+    r2 = Rugs('persian rugs', 1500)
+    r3 = Rugs('persian rugs', 1100)
+
+    adapter = PriceAdapter(rate=20000)
+
+    rugs = [r1, r2, r3]
+
+    for rug in rugs:
+        print(f"{rug._name}:{adapter.exchange(rug)}")
