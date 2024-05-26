2e1e84247c3dc5f6e9aa121a6d89c456a48fe18d
MrRezoo <rezam578@gmail.com>
2024-05-27 00:19:21 +0330

    Chore(Django): commandline answer

---
+++ Django/CommandLine/Answer/commandline/career/management/commands/collectCompany.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+from career.models import Company
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Collects information of all companies and writes to a csv file.'
+
+    def handle(self, *args, **options):
+        companies = Company.objects.all()
+        filename = 'company.csv'
+        filepath = os.path.join(settings.BASE_DIR, filename)
+
+        with open(filepath, mode='w') as csvfile:
+            writer = csv.writer(csvfile)
+            for company in companies:
+                writer.writerow([company.name, company.email, company.phone])
---
+++ Django/CommandLine/Answer/commandline/career/models.py
@@ -0,0 +1,16 @@
+from career.utils import PhoneValidator
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=14, validators=[PhoneValidator])
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+    def __str__(self):
+        return self.name
