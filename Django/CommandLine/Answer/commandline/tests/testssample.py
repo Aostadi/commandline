8a2b05a840e9991b89bc43f9ac7af428b57eef61
MrRezoo <rezam578@gmail.com>
2024-05-30 17:10:39 +0330

    Chore(Django): Data Refactor

--- Django/CommandLine/Answer/commandline/tests/testssample.py
+++ Django/CommandLine/Answer/commandline/tests/testssample.py
@@ -12,33 +12,33 @@ class AddTests(TestCase):
         err = StringIO()
         sys.stdin = StringIO('Snap\nSnap@email.com\n09122201202\nOnline Taxi\n')
         management.call_command('addCompany', stderr=err)
-        quera = Company.objects.get(name='Snap')
-        self.assertEqual(quera.email, 'Snap@email.com')
-        self.assertEqual(quera.phone, '09122201202')
-        self.assertEqual(quera.description, 'Online Taxi')
+        seven_learn = Company.objects.get(name='Snap')
+        self.assertEqual(seven_learn.email, 'Snap@email.com')
+        self.assertEqual(seven_learn.phone, '09122201202')
+        self.assertEqual(seven_learn.description, 'Online Taxi')
         self.assertEqual(err.getvalue(), '')
 
 
 class EditTest(TestCase):
     def setUp(self):
-        Company.objects.create(name='Quera', email="quera@email.com", phone="09122201202",
+        Company.objects.create(name='7learn', email="7learn@email.com", phone="09122201202",
                                description="a place for programmers")
 
     def test_success_edit(self):
         try:
-            management.call_command('editCompany', 'Quera', name='quera', email="Quera@email.com",
-                                    phone="+989112201202", description="Programming Contest")
+            management.call_command('editCompany', '7learn', name='7learn', email="7learn@email.com",
+                                    phone="+989112201202", description="Coding Competition")
         except:
             self.fail("unexpected CommandError!")
-        quera = Company.objects.get(name="quera")
-        self.assertEqual(quera.email, "Quera@email.com")
-        self.assertEqual(quera.phone, "+989112201202")
-        self.assertEqual(quera.description, "Programming Contest")
+        seven_learn = Company.objects.get(name="7learn")
+        self.assertEqual(seven_learn.email, "7learn@email.com")
+        self.assertEqual(seven_learn.phone, "+989112201202")
+        self.assertEqual(seven_learn.description, "Coding Competition")
 
 
 class RemoveTest(TestCase):
     def setUp(self):
-        self.quera = Company.objects.create(name='Quera', email="quera@email.com", phone="09122201202",
+        self.seven_learn = Company.objects.create(name='7learn', email="7learn@email.com", phone="09122201202",
                                             description="a place for programmers")
         self.yektanet = Company.objects.create(name='yektanet', email="Yektanet@email.com", phone="09122201203")
         self.snap = Company.objects.create(name='snap', email="snap@email.com", phone="09122201204")
